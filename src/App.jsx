f73bec8568f17163aff03a3923f8c19d3b60bd73
Harishjj03 <harishbalajj@gmail.com>
2026-03-10 13:40:11 +0530

    success

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Student from "./student";
+
+function App() {
+  return (
+    <div>
+      <h1>Student Details</h1>
+
+      <Student name="Harish" course="Modern Web Development" />
+      <Student name="Arun" course="React JS" />
+      <Student name="Kumar" course="JavaScript" />
+
+    </div>
+  );
+}
+
+export default App;
